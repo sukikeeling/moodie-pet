3c4f54baf7ee50e984dd510f7b34521a84dc6e8f
sukikeeling <sukikeeling@users.noreply.github.com>
2026-08-18 21:58:00 +0800

    feat(v1.6.0): 粒子旋转风暴变身/胶囊Wink表情/开源bloub云朵还原/10色专属台词/气泡框优化

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.GROKBOT_ORIGINAL = {
-EXPRESSIONS: [
+  EXPRESSIONS: [
               [
                 [
                   [130.36, 45.98],
@@ -2550,173 +2550,621 @@ EXPRESSIONS: [
                   [153.28, 127.58],
                 ],
               ],
+              [
+                [
+                  [83.5, 133.7],
+                  [83.82, 130.72],
+                  [84.77, 127.88],
+                  [86.29, 125.31],
+                  [88.33, 123.12],
+                  [90.79, 121.41],
+                  [93.56, 120.27],
+                  [96.5, 119.74],
+                  [99.5, 119.84],
+                  [102.4, 120.58],
+                  [105.07, 121.92],
+                  [107.4, 123.8],
+                  [109.28, 126.14],
+                  [110.62, 128.81],
+                  [111.36, 131.71],
+                  [111.5, 134.71],
+                  [111.5, 137.71],
+                  [111.5, 140.71],
+                  [111.5, 143.7],
+                  [111.5, 146.7],
+                  [111.5, 149.7],
+                  [111.5, 152.7],
+                  [111.5, 155.7],
+                  [111.5, 158.7],
+                  [111.5, 161.7],
+                  [111.18, 164.68],
+                  [110.23, 167.52],
+                  [108.71, 170.09],
+                  [106.67, 172.28],
+                  [104.21, 173.99],
+                  [101.44, 175.13],
+                  [98.5, 175.66],
+                  [95.5, 175.56],
+                  [92.6, 174.82],
+                  [89.93, 173.48],
+                  [87.6, 171.6],
+                  [85.72, 169.26],
+                  [84.38, 166.59],
+                  [83.64, 163.69],
+                  [83.5, 160.69],
+                  [83.5, 157.69],
+                  [83.5, 154.69],
+                  [83.5, 151.7],
+                  [83.5, 148.7],
+                  [83.5, 145.7],
+                  [83.5, 142.7],
+                  [83.5, 139.7],
+                  [83.5, 136.7],
+                ],
+                [
+                  [183.2, 142],
+                  [185.7, 142.4],
+                  [187.96, 143.57],
+                  [189.73, 145.38],
+                  [190.85, 147.66],
+                  [191.2, 150.17],
+                  [190.74, 152.66],
+                  [189.53, 154.89],
+                  [187.68, 156.63],
+                  [185.38, 157.7],
+                  [182.86, 158],
+                  [180.31, 158],
+                  [177.77, 158],
+                  [175.22, 158],
+                  [172.67, 158],
+                  [170.12, 158],
+                  [167.58, 158],
+                  [165.03, 158],
+                  [162.48, 158],
+                  [159.94, 158],
+                  [157.39, 158],
+                  [154.84, 158],
+                  [152.29, 158],
+                  [149.75, 158],
+                  [147.2, 158],
+                  [144.7, 157.6],
+                  [142.44, 156.43],
+                  [140.67, 154.62],
+                  [139.55, 152.34],
+                  [139.2, 149.83],
+                  [139.66, 147.34],
+                  [140.87, 145.11],
+                  [142.72, 143.37],
+                  [145.02, 142.3],
+                  [147.54, 142],
+                  [150.09, 142],
+                  [152.63, 142],
+                  [155.18, 142],
+                  [157.73, 142],
+                  [160.28, 142],
+                  [162.82, 142],
+                  [165.37, 142],
+                  [167.92, 142],
+                  [170.46, 142],
+                  [173.01, 142],
+                  [175.56, 142],
+                  [178.11, 142],
+                  [180.65, 142],
+                ],
+              ],
             ],
 GROUPS: {
-              'Cycle de vie': [
-                'sleeping',
-                'waking',
-                'idle',
-                'listening',
-                'thinking',
-                'searching',
-                'working',
-              ],
-              Réactions: [
-                'excited',
-                'surprised',
-                'suspicious',
-                'angry',
-                'drowsy',
-                'happy',
-                'curious',
-                'confused',
-                'bored',
-                'proud',
-                'shy',
-                'sad',
-                'laughing',
-                'scared',
-                'playful',
-                'celebrate',
-              ],
-              'Morphes agent': ['orbit', 'radar', 'progress'],
-              'Cycle produit': [
-                'spawning',
-                'humming',
-                'loading',
-                'dictating',
-                'writing',
-                'sending',
-                'receiving',
-                'uploading',
-                'notifying',
-                'alerting',
-                'dragging',
-                'bouncing',
-                'powering-down',
-              ],
-            },
+  "Cycle de vie": [
+    "sleeping",
+    "waking",
+    "idle",
+    "listening",
+    "thinking",
+    "searching",
+    "working"
+  ],
+  "Réactions": [
+    "excited",
+    "surprised",
+    "suspicious",
+    "angry",
+    "drowsy",
+    "happy",
+    "curious",
+    "confused",
+    "bored",
+    "proud",
+    "shy",
+    "sad",
+    "laughing",
+    "scared",
+    "playful",
+    "celebrate"
+  ],
+  "Morphes agent": [
+    "orbit",
+    "radar",
+    "progress"
+  ],
+  "Cycle produit": [
+    "spawning",
+    "humming",
+    "loading",
+    "dictating",
+    "writing",
+    "sending",
+    "receiving",
+    "uploading",
+    "notifying",
+    "alerting",
+    "dragging",
+    "bouncing",
+    "powering-down"
+  ]
+},
 POOLS: {
-              sleeping: [13, 22, 4],
-              waking: [13],
-              idle: [0, 8],
-              listening: [10, 1, 19],
-              thinking: [8, 16, 14, 17, 5],
-              searching: [15, 9, 3, 20, 12, 18],
-              working: [7, 16, 11, 10],
-              excited: [2, 17, 21, 3, 11],
-              surprised: [3, 21],
-              suspicious: [14, 5, 23],
-              angry: [7, 16],
-              drowsy: [4, 22, 13],
-              happy: [2, 11, 17, 19],
-              curious: [3, 21, 0, 15],
-              confused: [14, 5, 8],
-              bored: [4, 22, 0],
-              proud: [15, 8, 2],
-              shy: [0, 24, 13],
-              sad: [4, 13, 22],
-              laughing: [2, 11, 17],
-              scared: [3, 21],
-              playful: [2, 17, 11, 8],
-              celebrate: [2, 8, 17],
-              orbit: [0, 8],
-              radar: [0, 8],
-              progress: [0, 8],
-              spawning: [3, 0],
-              humming: [0, 8],
-              loading: [0, 8],
-              dictating: [10, 1, 19],
-              sending: [0, 8],
-              receiving: [19, 0, 8],
-              uploading: [15, 9, 8],
-              writing: [15, 9],
-              notifying: [3, 21, 0],
-              alerting: [3, 21],
-              bouncing: [2, 17],
-              dragging: [3, 15, 0],
-              'powering-down': [13, 22],
-            },
+  "sleeping": [
+    13,
+    22,
+    4
+  ],
+  "waking": [
+    13
+  ],
+  "idle": [
+    0,
+    8,
+    25
+  ],
+  "listening": [
+    10,
+    1,
+    19
+  ],
+  "thinking": [
+    8,
+    16,
+    14,
+    17,
+    5
+  ],
+  "searching": [
+    15,
+    9,
+    3,
+    20,
+    12,
+    18
+  ],
+  "working": [
+    7,
+    16,
+    11,
+    10
+  ],
+  "excited": [
+    2,
+    17,
+    21,
+    3,
+    11,
+    25
+  ],
+  "surprised": [
+    3,
+    21
+  ],
+  "suspicious": [
+    14,
+    5,
+    23
+  ],
+  "angry": [
+    7,
+    16
+  ],
+  "drowsy": [
+    4,
+    22,
+    13
+  ],
+  "happy": [
+    2,
+    11,
+    17,
+    19,
+    25
+  ],
+  "curious": [
+    3,
+    21,
+    0,
+    15
+  ],
+  "confused": [
+    14,
+    5,
+    8
+  ],
+  "bored": [
+    4,
+    22,
+    0
+  ],
+  "proud": [
+    15,
+    8,
+    2,
+    25
+  ],
+  "shy": [
+    0,
+    24,
+    13,
+    25
+  ],
+  "sad": [
+    4,
+    13,
+    22
+  ],
+  "laughing": [
+    2,
+    11,
+    17
+  ],
+  "scared": [
+    3,
+    21
+  ],
+  "playful": [
+    2,
+    17,
+    11,
+    8,
+    25
+  ],
+  "celebrate": [
+    2,
+    8,
+    17
+  ],
+  "orbit": [
+    0,
+    8
+  ],
+  "radar": [
+    0,
+    8
+  ],
+  "progress": [
+    0,
+    8
+  ],
+  "spawning": [
+    3,
+    0
+  ],
+  "humming": [
+    0,
+    8
+  ],
+  "loading": [
+    0,
+    8
+  ],
+  "dictating": [
+    10,
+    1,
+    19
+  ],
+  "sending": [
+    0,
+    8
+  ],
+  "receiving": [
+    19,
+    0,
+    8
+  ],
+  "uploading": [
+    15,
+    9,
+    8
+  ],
+  "writing": [
+    15,
+    9
+  ],
+  "notifying": [
+    3,
+    21,
+    0
+  ],
+  "alerting": [
+    3,
+    21
+  ],
+  "bouncing": [
+    2,
+    17
+  ],
+  "dragging": [
+    3,
+    15,
+    0
+  ],
+  "powering-down": [
+    13,
+    22
+  ]
+},
 EXPR_CADENCE: {
-              sleeping: [6000, 10000],
-              waking: [800, 800],
-              idle: [9000, 16000],
-              listening: [2800, 5000],
-              thinking: [2000, 3600],
-              searching: [1000, 1800],
-              working: [1800, 3200],
-              excited: [1100, 2000],
-              surprised: [2500, 4000],
-              suspicious: [2600, 4500],
-              angry: [2200, 3800],
-              drowsy: [4000, 8000],
-              happy: [2500, 4500],
-              curious: [1800, 3200],
-              confused: [2200, 3800],
-              bored: [3500, 6000],
-              proud: [3500, 6000],
-              shy: [3000, 5500],
-              sad: [4000, 7000],
-              laughing: [1200, 2400],
-              scared: [900, 1800],
-              playful: [1500, 3000],
-              celebrate: [1400, 2600],
-              orbit: [4000, 8000],
-              radar: [4000, 8000],
-              progress: [4000, 8000],
-              spawning: [1200, 1200],
-              humming: [5000, 9000],
-              loading: [6000, 10000],
-              dictating: [4000, 8000],
-              sending: [4000, 8000],
-              receiving: [4000, 8000],
-              uploading: [4000, 8000],
-              writing: [4000, 8000],
-              notifying: [1500, 2600],
-              alerting: [2000, 3600],
-              bouncing: [3000, 6000],
-              dragging: [1600, 3000],
-              'powering-down': [6000, 9000],
-            },
+  "sleeping": [
+    6000,
+    10000
+  ],
+  "waking": [
+    800,
+    800
+  ],
+  "idle": [
+    9000,
+    16000
+  ],
+  "listening": [
+    2800,
+    5000
+  ],
+  "thinking": [
+    2000,
+    3600
+  ],
+  "searching": [
+    1000,
+    1800
+  ],
+  "working": [
+    1800,
+    3200
+  ],
+  "excited": [
+    1100,
+    2000
+  ],
+  "surprised": [
+    2500,
+    4000
+  ],
+  "suspicious": [
+    2600,
+    4500
+  ],
+  "angry": [
+    2200,
+    3800
+  ],
+  "drowsy": [
+    4000,
+    8000
+  ],
+  "happy": [
+    2500,
+    4500
+  ],
+  "curious": [
+    1800,
+    3200
+  ],
+  "confused": [
+    2200,
+    3800
+  ],
+  "bored": [
+    3500,
+    6000
+  ],
+  "proud": [
+    3500,
+    6000
+  ],
+  "shy": [
+    3000,
+    5500
+  ],
+  "sad": [
+    4000,
+    7000
+  ],
+  "laughing": [
+    1200,
+    2400
+  ],
+  "scared": [
+    900,
+    1800
+  ],
+  "playful": [
+    1500,
+    3000
+  ],
+  "celebrate": [
+    1400,
+    2600
+  ],
+  "orbit": [
+    4000,
+    8000
+  ],
+  "radar": [
+    4000,
+    8000
+  ],
+  "progress": [
+    4000,
+    8000
+  ],
+  "spawning": [
+    1200,
+    1200
+  ],
+  "humming": [
+    5000,
+    9000
+  ],
+  "loading": [
+    6000,
+    10000
+  ],
+  "dictating": [
+    4000,
+    8000
+  ],
+  "sending": [
+    4000,
+    8000
+  ],
+  "receiving": [
+    4000,
+    8000
+  ],
+  "uploading": [
+    4000,
+    8000
+  ],
+  "writing": [
+    4000,
+    8000
+  ],
+  "notifying": [
+    1500,
+    2600
+  ],
+  "alerting": [
+    2000,
+    3600
+  ],
+  "bouncing": [
+    3000,
+    6000
+  ],
+  "dragging": [
+    1600,
+    3000
+  ],
+  "powering-down": [
+    6000,
+    9000
+  ]
+},
 BLINK: {
-              sleeping: null,
-              waking: null,
-              idle: [6000, 14000],
-              listening: [3000, 7000],
-              thinking: [3500, 7000],
-              searching: [1600, 4000],
-              working: [2800, 5500],
-              excited: [2000, 4000],
-              surprised: [1800, 3500],
-              suspicious: [4500, 8000],
-              angry: [3500, 7000],
-              drowsy: null,
-              happy: [2500, 5000],
-              curious: [2500, 5500],
-              confused: [2800, 5500],
-              bored: [4000, 8000],
-              proud: [3500, 7000],
-              shy: [3000, 6000],
-              sad: [4000, 8000],
-              laughing: [2500, 5000],
-              scared: [1200, 3000],
-              playful: [2000, 4500],
-              celebrate: [2200, 4500],
-              orbit: null,
-              radar: null,
-              progress: null,
-              spawning: null,
-              humming: [4000, 8000],
-              loading: null,
-              dictating: null,
-              sending: null,
-              receiving: null,
-              uploading: null,
-              writing: null,
-              notifying: [2000, 4000],
-              alerting: null,
-              bouncing: null,
-              dragging: [2200, 4500],
-              'powering-down': null,
-            }
-};;
+  "sleeping": null,
+  "waking": null,
+  "idle": [
+    6000,
+    14000
+  ],
+  "listening": [
+    3000,
+    7000
+  ],
+  "thinking": [
+    3500,
+    7000
+  ],
+  "searching": [
+    1600,
+    4000
+  ],
+  "working": [
+    2800,
+    5500
+  ],
+  "excited": [
+    2000,
+    4000
+  ],
+  "surprised": [
+    1800,
+    3500
+  ],
+  "suspicious": [
+    4500,
+    8000
+  ],
+  "angry": [
+    3500,
+    7000
+  ],
+  "drowsy": null,
+  "happy": [
+    2500,
+    5000
+  ],
+  "curious": [
+    2500,
+    5500
+  ],
+  "confused": [
+    2800,
+    5500
+  ],
+  "bored": [
+    4000,
+    8000
+  ],
+  "proud": [
+    3500,
+    7000
+  ],
+  "shy": [
+    3000,
+    6000
+  ],
+  "sad": [
+    4000,
+    8000
+  ],
+  "laughing": [
+    2500,
+    5000
+  ],
+  "scared": [
+    1200,
+    3000
+  ],
+  "playful": [
+    2000,
+    4500
+  ],
+  "celebrate": [
+    2200,
+    4500
+  ],
+  "orbit": null,
+  "radar": null,
+  "progress": null,
+  "spawning": null,
+  "humming": [
+    4000,
+    8000
+  ],
+  "loading": null,
+  "dictating": null,
+  "sending": null,
+  "receiving": null,
+  "uploading": null,
+  "writing": null,
+  "notifying": [
+    2000,
+    4000
+  ],
+  "alerting": null,
+  "bouncing": null,
+  "dragging": [
+    2200,
+    4500
+  ],
+  "powering-down": null
+}
+};
--- verify.js
+++ verify.js
@@ -5,7 +5,7 @@ sb.console = console;
 vm.createContext(sb);
 vm.runInContext(data, sb);
 const D = sb.window.GROKBOT_ORIGINAL;
-console.log('EXPRESSIONS:', D.EXPRESSIONS.length, '(expect 25)');
+console.log('EXPRESSIONS:', D.EXPRESSIONS.length, '(expect 26)');
 console.log('states:', Object.keys(D.POOLS).length, '(expect 39)');
 console.log('cadence states:', Object.keys(D.EXPR_CADENCE).length, '(expect 39)');
 console.log('expr0 rings:', D.EXPRESSIONS[0].length, 'ring0 pts:', D.EXPRESSIONS[0][0].length, 'ring1 pts:', D.EXPRESSIONS[0][1].length);
@@ -13,5 +13,5 @@ console.log('expr0 rings:', D.EXPRESSIONS[0].length, 'ring0 pts:', D.EXPRESSIONS
 let ok = true;
 D.EXPRESSIONS.forEach((ex, i) => { if (ex.length !== 2 || ex[0].length < 3 || ex[1].length < 3) { ok = false; console.log('bad expr', i); } });
 // every pool references valid expression indices
-Object.entries(D.POOLS).forEach(([k, p]) => p.forEach(idx => { if (idx < 0 || idx >= 25) { ok = false; console.log('bad pool idx', k, idx); } }));
+Object.entries(D.POOLS).forEach(([k, p]) => p.forEach(idx => { if (idx < 0 || idx >= D.EXPRESSIONS.length) { ok = false; console.log('bad pool idx', k, idx); } }));
 console.log('structural check:', ok ? 'PASS' : 'FAIL');
